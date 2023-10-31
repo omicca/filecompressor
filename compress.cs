38e5da61ce655a7644b3a0a7c51d89eafa732973
Oliver Michaelsen <omicha19@student.aau.dk>
2023-10-31 13:58:22 +0100

    Added basic reading of text files Basic setup for compression

---
+++ compress.cs
@@ -0,0 +1,62 @@
+﻿namespace FileCompressor;
+
+public class Compress
+{
+    private class HuffmannCode
+    {
+        //construct huffmann code
+        
+        
+    }
+     
+    public string[] ReadFile()
+    {
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string filePath = Path.Combine(currentDirectory, @"..\..\..\input\");
+        string fullPath = Path.GetFullPath(filePath);
+
+        string[] txtFiles = new string[2];
+        try
+        {
+            var dataFiles = Directory.EnumerateFiles(fullPath, "*.txt", SearchOption.TopDirectoryOnly);
+
+            int i = 0;
+            foreach (var files in dataFiles)
+            {
+                txtFiles[i] = files;
+                i++;
+                i++;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return txtFiles;
+    }
+     
+    public void CompressTextFile(string file)
+    {
+        if (File.Exists(file))
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(file);
+                 
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"{e}: Error occured while reading file.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("File does not exist.");
+        }
+    }
+}
---
+++ decompress.cs
@@ -0,0 +1,6 @@
+﻿namespace FileCompressor;
+
+public class Decompress
+{
+    //decompress files
+}
